396bf901113fb3071093c1854aefd209df6055f1
Mahesh Venkitachalam <info@electronut.in>
2015-12-21 12:51:21 +0530

    updated

--- attiny85-mma7660-AS/attiny85-mma7660-AS/main.c
+++ attiny85-mma7660-AS/attiny85-mma7660-AS/main.c
@@ -11,12 +11,46 @@
 #include "i2cmaster.h"
 
 
+#define SLAVE_ADDR  0x4C
+
+unsigned char ret;
+uint8_t data;
+
 int main()
 {
-	DDRB = 0b00001000;
+	
+	
+	i2c_init();
+
+	//DDRB = 0b00001000;
     /* Replace with your application code */
     while (1) 
-    {
+    {	
+		
+		
+#if 0
+		ret = i2c_start((0x4C << 1)|0x0);
+		i2c_write(0x04);
+		i2c_rep_start((0x4C << 1)|0x1);
+		data = i2c_readNak();
+#endif
+
+		ret = i2c_start((0x4C << 1)|0x0);
+		i2c_write(0x07);
+		i2c_write(0b00011001);
+		i2c_stop();
+		
+		_delay_ms(1);
+		
+		ret = i2c_start((0x4C << 1)|0x0);
+		i2c_write(0x02);
+		i2c_rep_start((0x4C << 1)|0x1);
+		data = i2c_readNak();
+		
+		ret = 5;
+		//i2c_stop();
+		
+		/*
 		// flash# 1:
 		// set PB3 high
 		PORTB = 0b00001000;
@@ -24,14 +58,9 @@ int main()
 		// set PB3 low
 		PORTB = 0b00000000;
 		_delay_ms(20);
-
-		// flash# 2:
-		// set PB3 high
-		PORTB = 0b00001000;
-		_delay_ms(200);
-		// set PB3 low
-		PORTB = 0b00000000;
-		_delay_ms(200);
+		*/
+		
+		_delay_ms(250);
 		
     }
 }
